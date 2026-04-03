9d0dc681f4ee54f8fe66bdceccaf0665f3c3346c
Anubhavg04 <anubhavgup21@gmail.com>
2026-04-03 16:28:37 +0530

    fix: add staleTimes caching for faster page navigation

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
+    staleTimes:{
+      dynamic: 30,
+      static: 180,
+    },
+  },
 };
 
 export default nextConfig;
